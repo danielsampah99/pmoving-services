9131a91f8c5d5888f96e9fe2897d09dc702a0637
rafissaka <151390308+rafissaka@users.noreply.github.com>
2025-02-22 13:40:23 +0000

    New Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import ExpertSection from "@/components/Expert";
 import Header from "@/components/Header";
 import HeroSection from "@/components/Hero";
-import Mission from "@/components/Mission";
+import MovingSection from "@/components/Mission";
 import ServicesSection from "@/components/ServicesSection";
 
 
@@ -10,6 +11,8 @@ export default function Home() {
       <Header />
       <HeroSection/>
       <ServicesSection/>
+      <MovingSection/>
+      <ExpertSection/>
    
     </div>
   );
