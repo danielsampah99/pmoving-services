2db739d46cddf4d14152319ba4e9b24402739fbb
rafissaka <151390308+rafissaka@users.noreply.github.com>
2025-02-28 19:40:54 +0000

    Blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import BlogComponent from '@/components/Blog/BlogComponent'
+import React from 'react'
+
+const Blog = () => {
+  return (
+   
+   <BlogComponent/>
+  
+  )
+}
+
+export default Blog
