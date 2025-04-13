51f00bf99623dcae3dedac29e22b3b14d0df215b
Daniel Sampah <danielsampah99@gmail.com>
2025-04-13 14:34:52 +0000

    Redesign thank-you page with improved layout and content

--- src/app/(app)/thank-you/page.tsx
+++ src/app/(app)/thank-you/page.tsx
@@ -1,29 +1,45 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { OurLocations } from "@/components/OurLocations";
 
 
 export default function ThankYouPage() {
 	return (
-		<main className='p-6 lg:px-8 mx-auto min-h-[calc(100dvh-92px+var(--spacing)*6)] relative border border-solid border-red-500 bg-moving-gray'>
-			<section className='max-w-7xl '>
-				<div className='absolute top-10 -left-8 sm:-top-30 md:-top-64 sm:left-4'>
-					<Image
-						src="/thank-you-truck.webp"
-						alt='Our Moving Truck'
-						width={1000}
-						height={1000}
+		<main className='p-6 isolate lg:px-8 mx-auto relative bg-moving-gray'>
+			{/* min-h-[calc(100dvh-92px+var(--spacing)*6)] */}
+			<div className="mx-auto max-w-7xl">
+				<div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
+					<img
+						alt="our moving trucks"
+						src="https://lirp.cdn-website.com/b0f4adc5/dms3rep/multi/opt/IMG_2716c-1920w.jpg"
+						className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
 					/>
-				</div>
+					<div className="w-full flex-auto">
+						<h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl capitalize">Thank you for contacting us</h2>
+						<p className="mt-6 text-lg leading-8 text-gray-300">
+							One of our moving consultants will contact you regarding your move quote. If you would like to get a hold of us sooner, please call us directly on <a href='tel:6517575135' className='text-moving-yellow/70 hover:text-moving-yellow hover:underline underline-offset-2'>(651) 757-5135</a>
+						</p>
 
-				<div className='absolute bottom-30 sm:bottom-10 lg:bottom-0 transition-all delay-150  lg:gap-y-3 sm:top-1/3 sm:right-10 flex flex-col items-center justify-center sm:gap-y-1.5 lg:max-w-1/3'>
-					<h5 className='text-white font-semibold lg:whitespace-nowrap text-3xl lg:text-4xl'>Thank you for <span className='text-moving-yellow capitalize font-semibold'>Contacting Us</span></h5>
-					<p className='text-gray-200 font-medium'>One of our moving consultants will contact regarding your move quote.</p>
-					<Link href='/' className='bg-moving-yellow/80 hover:bg-moving-yellow font-semibold lg:text-lg shadow-md px-4 py-2 lg:py-3 lg:px-6 text-base text-gray-900 rounded-lg hover:shadow-lg hover:font-bold'>Back to home page</Link>
+						<div className="mt-10 flex">
+							<a href="/#reviews" className="group text-base font-semibold leading-6 text-moving-yellow/75">
+								Check our our reviews <span className="group-hover:translate-x-4 group-hover:ease-linear group-hover:scale-150 group-hover:text-moving-yellow transition-all delay-100 duration-300" aria-hidden="true">&rarr;</span>
+							</a>
+						</div>
+					</div>
 				</div>
-			</section>
+			</div>
+			<div
+				aria-hidden="true"
+				className="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
+			>
+				<div
+					style={{
+						clipPath:
+							'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
+					}}
+					className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[var(--color-moving-yellow)/50] to-[var(--color-moving-yellow)] opacity-25"
+				/>
+			</div>
+
+			<OurLocations />
 		</main>
 	)
 }
-
-
-// https://www.pmovingservices.com/photo-gallery#&gid=1975769568&pid=5
