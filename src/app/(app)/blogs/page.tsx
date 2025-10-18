bcc5e2aa7221575415198c32dcf506812a919842
Daniel Sampah <danielsampah99@gmail.com>
2025-10-18 18:58:51 +0000

    feat: update open-graph sections of meta tags

--- src/app/(app)/blogs/page.tsx
+++ src/app/(app)/blogs/page.tsx
@@ -19,25 +19,25 @@ export const metadata: Metadata = {
 		"moving blog"
 	],
 	openGraph: {
-		title: "Moving Services Blogs - Tips, Tricks, and Advice",
+		title: "Moving Tips, Guides and Costs | Premium Moving Services Blog",
 		description:
-			"Stay updated with the latest trends, tips, and advice in the moving industry. Our blog covers everything from packing hacks to choosing the right mover.",
-		url: BASE_URL,
+			"Read everything essential about expert tips, cost breakdowns, checklists and advice for the local and long distance moves",
+		url: `${BASE_URL}/blogs`,
 		type: "website",
 		images: [
 			{
 				url: `${BASE_URL}/logo.svg`,
 				width: 1200,
 				height: 630,
-				alt: "Moving Services Blog",
+				alt: "Moving Services Blog"
 			},
 		],
 	},
 	twitter: {
 		card: "summary_large_image",
-		title: "Moving Services Blogs - Tips, Tricks, and Advice",
+		title: "Moving Tips, Guides and Costs | Premium Moving Services Blog",
 		description:
-			"Stay updated with the latest trends, tips, and advice in the moving industry. Our blog covers everything from packing hacks to choosing the right mover.",
+			"Read everything essential about expert tips, cost breakdowns, checklists and advice for the local and long distance moves",
 		images: [`${BASE_URL}/logo.svg`],
 	},
 };
