3fd7e2fa3cf4e240b8a164a7c08d0b15dec82917
danielsampah99 <danielsampah99@gmail.com>
2026-05-27 09:55:49 +0000

    fix sizing issue on the testimonial

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils";
 import type { FlatIcon } from "./flaticons";
 import { AffordableIcon } from "./icons/affordable-icon";
 import { InsuranceIcon } from "./icons/insurance-icon";
@@ -7,22 +8,22 @@ import type { FC } from "react";
 
 export const Testimonial = () => {
 	return (
-		<div className="bg-white pb-8 pt-12">
-			<div className="bg-moving-gray pb-10 sm:pb-24 xl:pb-0">
+		<div className="bg-white pt-24 pb-16 sm:pt-32 sm:pb-24 xl:pb-32">
+			<div className="bg-moving-gray pb-20 sm:pb-24 xl:pb-0">
 				<div className="mx-auto flex max-w-7xl flex-col items-center gap-x-8 gap-y-10 px-6 sm:gap-y-8 lg:px-8 xl:flex-row xl:items-stretch">
-					<div className="-mt-8 w-full max-w-2xl sm:max-w-lg sm:-mt-16 md:-mt-8 xl:-mb-8 xl:w-96 xl:flex-none">
-						<div className="relative aspect-[2/1] h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
-							<div className="absolute inset-0 h-3/4 w-2/3 rounded-2xl p-3 bg-moving-yellow object-cover shadow-2xl">
+					<div className="-mt-16 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
+						<div className="relative aspect-2/1 h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
+							<div className="absolute inset-0 size-full h-65 rounded-2xl p-3 bg-moving-yellow object-cover shadow-2xl">
 								<img
 									src="/bbb-logo.png"
 									alt="Better Business Bureau's Logo"
-									className="size-full fill-white z-[9000]"
+									className="h-60 w-auto mx-auto z-9000 object-cover"
 								/>
 							</div>
 						</div>
 					</div>
-					<div className="w-full max-w-2xl xl:max-w-none xl:flex-auto xl:px-8 xl:py-3">
-						<figure className="relative isolate pt-6 sm:pt-3 xl:pt-4">
+					<div className="w-full max-w-2xl xl:max-w-none xl:flex-auto xl:px-8 xl:py-6">
+						<figure className="relative isolate py-3 space-y-16 lg:space-y-8 sm:pt-3 xl:pt-4">
 							<blockquote className="text-xl font-semibold leading-8 text-white sm:text-xl sm:leading-9">
 								<ul className="flex flex-row flex-wrap items-center justify-between w-full gap-x-2">
 									<NameAndIcon text="Trusted" icon={TrustIcon} />
@@ -30,11 +31,25 @@ export const Testimonial = () => {
 									<NameAndIcon text="Reliable" icon={ReliableIcon} />
 								</ul>
 							</blockquote>
-							<figcaption className="mt-8 text-base flex flex-row flex-wrap border-t border-solid sm:text-xl border-gray-200 pt-4 items-center justify-between gap-y-1 text-white">
-								<NameAndIcon text="Licensed" icon={InsuranceIcon} />
-								<NameAndIcon text="Insured" icon={InsuranceIcon} />
-								<NameAndIcon text="Bonded" icon={InsuranceIcon} />
-							</figcaption>
+							<blockquote className="text-xl font-semibold leading-8 text-white sm:text-xl sm:leading-9 ">
+								<ul className="flex flex-wrap items-center justify-between w-full gap-2 text-moving-yellow">
+									<NameAndIcon
+										text="Licensed"
+										icon={InsuranceIcon}
+										iconClassName="fill-moving-yellow"
+									/>
+									<NameAndIcon
+										text="Bonded"
+										icon={InsuranceIcon}
+										iconClassName="fill-moving-yellow"
+									/>
+									<NameAndIcon
+										text="Insured"
+										icon={InsuranceIcon}
+										iconClassName="fill-moving-yellow"
+									/>
+								</ul>
+							</blockquote>
 						</figure>
 					</div>
 				</div>
@@ -46,15 +61,22 @@ export const Testimonial = () => {
 export type NameAndIconProps = {
 	text: string;
 	icon: FlatIcon;
+	iconClassName?: string;
 };
 
 export const NameAndIcon: FC<NameAndIconProps> = (props) => {
 	return (
-		<div className="group inline-flex hover:text-moving-yellow transition-colors delay-150 duration-300 items-center justify-normal gap-x-1 selection:bg-moving-yellow">
-			<span>
-				<props.icon className="size-6 group-hover:size-7 group-hover:fill-moving-yellow fill-white" />
-			</span>
-			<span>{props.text}</span>
+		<div className="group flex flex-col text-sm lg:text-base gap-1 hover:text-moving-yellow transition-colors delay-150 duration-300 items-center justify-start gap-x-1 selection:bg-moving-yellow">
+			<div>
+				<props.icon
+					className={cn(
+						"size-10 group-hover:fill-moving-yellow fill-white",
+						props.iconClassName,
+					)}
+				/>
+			</div>
+
+			<div>{props.text}</div>
 		</div>
 	);
 };
